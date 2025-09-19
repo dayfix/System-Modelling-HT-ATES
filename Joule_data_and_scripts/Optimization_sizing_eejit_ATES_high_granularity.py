084f92167899c1809368563c9c9ccc958a45d46c
David G <132259291+dayfix@users.noreply.github.com>
2025-09-19 08:46:02 +0200

    Added Joule_submission code

---
+++ Joule_data_and_scripts/Optimization_sizing_eejit_ATES_high_granularity.py
@@ -0,0 +1,172 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  9 08:55:01 2024
+
+@author: 6100430
+"""
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from scipy.signal import savgol_filter
+from scipy.interpolate import griddata
+from scipy.interpolate import interpn
+from ATES_obj import ATES_obj
+import joblib
+import pytest
+import time
+from main2 import geothermal, demand_class, heat_pump_ATES,gas_boiler, system,\
+    economic_analysis, LCOE_calc_Yang, CO2_emissions_calc, Solar_collector, system_plot
+from scipy.optimize import minimize
+from scipy.optimize import Bounds
+import matplotlib.ticker as ticker
+from matplotlib.lines import Line2D
+from tqdm import tqdm
+import multiprocessing as mp
+import random
+import traceback
+
+
+def Optim(x):
+    try:
+      x=x[0]
+      fname = "/path/to/storage/"+str(x[0])+" "+str(x[1])+" "+str(x[2])+" "+str(x[3])+" "+str(x[4])+" "+str(x[5])+" "+str(x[6])
+      if os.path.isfile(fname):
+          return 0,0
+      timestep = 3600*6
+      # demand = demand_class(T_in =75,T_out = 55,example_demand="Delft Total")
+      demand = demand_class(T_in =75,T_out = 55,example_demand=x[0])
+      gas = gas_boiler(lifetime=15,gas_price=0.055)
+      if x[1] == "Solar-HT-ATES":
+          df_weather = pd.read_parquet("/path/to/weatherfile/"+x[0])
+          output = np.array(df_weather["G(i)"])
+          heat_per_kWp = 600*sum(output)/1047954
+          solar = Solar_collector(peak_power = x[2], T_out = 75,heat_per_kWp=heat_per_kWp,output_array=output)
+          solar.capex = solar.capex*x[5]
+          solar.fixed_opex = solar.fixed_opex*x[5]
+          solar.var_opex=solar.var_opex*x[5]
+          
+          ATES = ATES_obj([solar],max_V = x[4],HP=None,thickness=50,CO2_elec=200,kh=10,ani=5,T_ground=15,N_wells=6,depth=130,lifetime=30)
+          ATES.fix_opex = ATES.fix_opex*x[6]
+          ATES.var_opex = ATES.var_opex*x[6]
+          ATES.capex = ATES.capex*x[6]
+          supply = [solar,ATES,gas]
+  
+      elif x[1] == "Geo-HT-ATES":
+          geo = geothermal(flow_rate = x[3],T_out=75,lifetime = 30,CO2_kg=12.5)
+          geo.costperkW = geo.costperkW * x[5]
+          geo.fixed_opex = geo.fixed_opex *x[5]
+          geo.var_opex = geo.var_opex *x[5]
+          ATES = ATES_obj([geo],max_V = x[4],HP=None,CO2_elec=200,thickness=50,kh=10,ani=5,T_ground=15,N_wells=6,depth=130,lifetime=30)
+          ATES.fix_opex = ATES.fix_opex*x[6]
+          ATES.var_opex = ATES.var_opex*x[6]
+          ATES.capex = ATES.capex*x[6]
+          supply = [geo,ATES,gas]
+  
+          #geo.capex = 22000000
+          #ATES.capex = 3408000-options_df_row["Subsidy amount ATES"]
+  
+      
+      #ATES.set_reff(0.85)
+  
+      # supply = [geo,ATES,gas]
+      result,df_flow = system(demand,supply,len_timestep=timestep, control = None)
+      
+      ATES_fixed = True
+      df_eco = economic_analysis(result, supply,disc_rate=0.05,incorporate_CO2=True,CO2_price=75,opex_ATES_fixed=ATES_fixed)
+      Network_length = 23 #km
+      capex_network = Network_length*1157*1000
+      opex_as_capex_network = 0.02
+      lifetime_network = 60
+      LCOE_Yang = LCOE_calc_Yang(result,supply,df_eco,lifetime_system=60,capex_network=capex_network,opex_network_perc=opex_as_capex_network,lifetime_network=lifetime_network)
+  
+      RES_lib = ["Geothermal well corrected","ATES corrected","Heat pump corrected", "Solar boiler corrected"]
+      RES_share = 0
+      for j in result.columns:
+          if any(RES in j for RES in RES_lib):
+              RES_share = RES_share+sum(result[j])
+              try:
+                  if not np.isnan(sum(result[j.replace('corrected',"percentage to storage")]*result[j.replace(" corrected"," production")])):
+                      RES_share = RES_share - sum(result[j.replace('corrected',"percentage to storage")]*result[j.replace(" corrected"," production")])
+                  pass
+              except:
+                  pass
+      RES_share = RES_share/sum(demand.data)
+      if hasattr(ATES,"Reff"):
+          pass
+      else:
+          ATES.Reff = 0
+      #print(df_eco)
+      sum_cost=0
+      for i in supply:
+          if np.isnan(df_eco.loc[i.name]["LCOE"]):
+              pass
+          else:
+              sum_cost = sum_cost + df_eco.loc[i.name]["LCOE"]*sum(result[str(i.name)+" corrected"])
+      df_eco["sum_cost"]=sum_cost
+      df_eco["RES_share"]=RES_share
+      df_CO2 = CO2_emissions_calc(result, supply, CO2_price=150)
+      df_eco["Yearly_CO2_emissions [kg]"]=df_CO2["CO2_emission [kg]"]
+      df_eco["Yearly_kWh"]=sum(demand.data)
+      df = pd.DataFrame(data={"Size_Geo": [x[3]],"Size_solar":[x[2]],"Size_ATES":[x[4]],"Config":[x[1]],"Demand":[x[0]],"Ratio": [x[5]],"Ratio_ATES":[x[6]],"System_LCOH": [LCOE_Yang],"RES":[RES_share],"HT-ATES efficiency":[ATES.Reff]})    
+      save_file = "/path/to/storage/"
+      df.to_parquet(save_file+str(x[0])+" "+str(x[1])+" "+str(x[2])+" "+str(x[3])+" "+str(x[4])+" "+str(x[5])+" "+str(x[6]))
+      return LCOE_Yang, RES_share
+    except:
+      """Except clause to show which inpus made the program crash and print the inputs"""
+      traceback.print_exc()
+      print("one crashed", flush=True)
+      return 
+
+
+
+granularity = 120
+max_geo = 2100
+max_HTATES = 3000
+max_solar = 450000
+config =["Solar-HT-ATES","Geo-HT-ATES"]
+Demand = ["Berlin","Chongqing","Bagdad"]
+ratio = [0.3333,0.5,1,2]
+ratio_ATES = [0.5,1,2]
+x_df=pd.DataFrame()
+x = pd.DataFrame(0,index = [0], columns = ["demand","config","solar size","geo size","HT-ATES size","ratio","HT-ATES ratio"])
+x_list=[]
+
+for z in Demand:
+    for q in ratio:
+        for j in range(granularity):
+            for k in range(granularity):
+                for i in config:
+                  for l in ratio_ATES:
+                    if i == "Solar-HT-ATES":
+                        if q==0.3333:
+                            continue
+                        else:
+                            x["demand"]=z
+                            x["config"]=i
+                            x["solar size"]=j*(max_solar/granularity)
+                            x["HT-ATES size"]=k*(max_HTATES/granularity)
+                            x["geo size"]=0
+                            x["ratio"] = q
+                            x["HT-ATES ratio"]=l
+                    elif i == "Geo-HT-ATES":
+                        x["demand"]=z
+                        x["config"]=i
+                        x["geo size"]=j*(max_geo/granularity)
+                        x["solar size"]=0
+                        x["HT-ATES size"]=k*(max_HTATES/granularity)
+                        x["ratio"] = q
+                        x["HT-ATES ratio"]=l
+                    x_list.append(np.array(x))
+
+
+print("prep done",flush=True)                                  
+n_cores = mp.cpu_count()    #Get core count
+if __name__ == "__main__":
+    pool = mp.Pool(n_cores)     #Use all cores for multiprocessing pool
+    #A,B = Optim(x_list[479])
+    random.shuffle(x_list)
+    results = pool.map_async(Optim, x_list)    #Run the parallell pool. Everything is saved in the function itself
+    results.get()
+    
